46464c66a5e42608cd684b7ed755181abd4c11f4
Lucas Pizoni Flores <lucas.324623@alunosatc.edu.br>
2025-03-31 16:33:29 -0300

    Update função 'add' da classe 'TaskList'

--- aula03-js/atividade/js/class/TaskList.js
+++ aula03-js/atividade/js/class/TaskList.js
@@ -11,4 +11,14 @@ export class TaskList{
     {
         return this.#list.length;
     }
+
+    // Adiciona uma tarefa ao final da lista - adicionarTarefa()
+    add(task)
+    {
+        if(task.constructor.name == 'Task'){
+            this.#list.push(task);
+        }else{
+            throw new Error("O parâmetro não é um objeto de classe 'Task'.");
+        }
+    }
 }
