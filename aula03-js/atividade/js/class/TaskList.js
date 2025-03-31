e45d955e666ee3182e66453d6e53a80e31171f7f
Lucas Pizoni Flores <lucas.324623@alunosatc.edu.br>
2025-03-31 16:32:49 -0300

    Update função 'legth' da classe 'TaskList'

--- aula03-js/atividade/js/class/TaskList.js
+++ aula03-js/atividade/js/class/TaskList.js
@@ -1,3 +1,14 @@
 export class TaskList{
-    
+    #limite;
+    #list = [];
+
+    constructor(limite = 25)
+    {
+        this.#limite = limite;
+    }
+
+    legth()
+    {
+        return this.#list.length;
+    }
 }
