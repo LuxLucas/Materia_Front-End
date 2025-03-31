5b90615d3426279e0b3dbc3c1db549f0386c8117
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-03-31 01:13:51 -0300

    Métodos geters e seters da classe User criados

--- aula03-js/atividade/js/class/User.js
+++ aula03-js/atividade/js/class/User.js
@@ -3,20 +3,65 @@ export class User{
 
     // Atributos privados
     #id;
-    #nome;
-    #idade;
+    #name;
+    #age;
 
-    // último 'id' instanciado pela classe
+    // Último 'id' instanciado pela classe
     static #lastId = 0;
 
-    newId(){
+    // Concatena o último 'id'
+    #generateId()
+    {
         User.#lastId++;
         return User.#lastId;
     }
 
-    constructor(nome, idade){
-        this.#id     = newId();
-        this.#nome   = nome;
-        this.#idade  = idade;
+    // Método construtor
+    constructor(name, age)
+    {
+        this.#id     = this.#generateId();
+        this.#name   = name;
+        this.#age  = age;
     }
+
+    // Funções 'geters'
+    getName()
+    {
+        return this.#name;
+    }
+
+    getage()
+    {
+        return this.#age;
+    }
+
+    getId()
+    {
+        return this.#id;
+    }
+
+    static getLastId()
+    {
+        return User.#lastId;
+    }
+
+    // Métodos 'seters'
+    setName(name)
+    {
+        if(typeof(name) == 'string'){
+            this.#name = name;
+        }else{
+            throw new Error("'name' deve ser uma string.");
+        }
+    }
+
+    setAge(age)
+    {
+        if(typeof(age) == 'number' && age % 1 == 0 && age >= 0){
+            this.#age = age;
+        }else{
+            throw new Error("'age' deve ser um inteiro positivo.");
+        }
+    }
+    
 }
