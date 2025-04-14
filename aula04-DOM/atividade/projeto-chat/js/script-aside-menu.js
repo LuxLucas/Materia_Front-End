88b9ee9685ea1c9e4fbf314065724e06a4b9504f
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-04-13 23:38:23 -0300

    Update: interações 'hover' do menu lateral adicionadas

--- aula04-DOM/atividade/projeto-chat/js/script-aside-menu.js
+++ aula04-DOM/atividade/projeto-chat/js/script-aside-menu.js
@@ -1,24 +1,52 @@
-let boxMenuButtons = document.querySelectorAll('.box-menu-button');
+let menuButtonsBox      = document.querySelectorAll('.menu-button-box');
+let buttonsOfSideMenu   = document.querySelectorAll('aside.aside-menu nav.menu button');
 
-for(let box of boxMenuButtons){
+function showBorderLeftMenuButton(HTMLElement){
+    HTMLElement.style.borderLeft   = '0.2em solid var(--focused-menu-icon)';
+    HTMLElement.style.borderRight  = '0.2em solid transparent';
+}
+
+function hiddenBorderLeftMenuButton(HTMLElement){
+    HTMLElement.style.border = 'none';
+}
+
+for(let box of menuButtonsBox){
     let borderLeftMenuButton    = box.children[0];
     let buttonFilter            = borderLeftMenuButton.children[0];
 
-    box.addEventListener('mouseenter', () => {        
-        borderLeftMenuButton.style.borderLeft   = '0.2em solid var(--focused-menu-icon)';
-        borderLeftMenuButton.style.borderRight  = '0.2em solid transparent';
-
+    box.addEventListener('mouseenter', () => {       
         for(let child of buttonFilter.children){
-            child.classList.toggle('hidden');
+            showBorderLeftMenuButton(borderLeftMenuButton);
+            if(!(child.tagName == 'IMG' && child.classList.contains('img-account-menu')) && !borderLeftMenuButton.classList.contains('border-active')){
+                child.classList.toggle('hidden');
+            }
         }
     });
 
     box.addEventListener('mouseleave', () => {
-        borderLeftMenuButton.style.border = 'none';
-
         for(let child of buttonFilter.children){
-            child.classList.toggle('hidden');
+            hiddenBorderLeftMenuButton(borderLeftMenuButton);
+            if(!(child.tagName == 'IMG' && child.classList.contains('img-account-menu')) && !borderLeftMenuButton.classList.contains('border-active')){
+                child.classList.toggle('hidden');
+            }
         }
     })
 }
 
+for(let button of buttonsOfSideMenu){
+    button.addEventListener('click', () => {
+        let parentButton = button.parentElement;
+        let activeBorderLeft = document.querySelectorAll('aside.aside-menu nav.menu div.left-edge-menu-button.border-active');
+
+        console.log(activeBorderLeft);
+        if(activeBorderLeft.length > 0){
+            activeBorderLeft.forEach((Element) => {
+                Element.classList.remove('border-active');
+                hiddenBorderLeftMenuButton(Element);
+            })
+        }
+
+        showBorderLeftMenuButton(parentButton);
+        parentButton.classList.add('border-active');
+    })
+}
